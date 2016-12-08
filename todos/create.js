09fdbba429d172d54cbbf18c950d58d671ceadd3
Chen Li <yichen.li0830@gmail.com>
2016-12-07 22:55:01 -0800

    Successfully implemented create lambda function

--- todos/create.js
+++ todos/create.js
@@ -4,7 +4,7 @@
 'use strict';
 
 let AWS = require("aws-sdk");
-
+let uuid = require("uuid");
 let docClient = new AWS.DynamoDB.DocumentClient();
 
 module.exports = (event, context, callback) => {
@@ -16,20 +16,34 @@ module.exports = (event, context, callback) => {
   if (typeof data.text !== 'string') {
     console.error("Invalid input: todo must be a string");
     callback(new Error('Couldn\'t create the todo item.'));
+    return;
   }
   
-  const response = {
-    statusCode: 200,
-    // body: JSON.stringify(event.body)
-    body: JSON.stringify(data)
+  const params = {
+    TableName: "todos",
+    Item: {
+      id: uuid.v4(),
+      text: data,
+      checked: false,
+      createdAt: timeStamp,
+      updatedAt: timeStamp,
+    }
   };
   
-  callback(null, response);
-  // const params = {
-  //   TableName: "todos",
-  //   Item: {
-  //     text: data
-  //   }
-  // }
-  
+  docClient.put(params, (err, data) => {
+    // handle potential errors
+    console.log("err", err);
+    console.log("data", data);
+    if (err) {
+      callback(new Error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2)));
+      return;
+    }
+    // item added Successfully
+    console.log("Added item:", JSON.stringify(data, null, 2));
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    };
+    callback(null, response);
+  });
 };
